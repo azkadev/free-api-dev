69333a553af2954b940f027789ef57e2777fc299
Azkadev <azkadev@pop-os.localdomain>
2021-11-18 09:04:26 +0700

    update

--- router/api/index.js
+++ router/api/index.js
@@ -1,5 +1,12 @@
 var express = require("express");
 var app = express.Router();
 
+app.get("/", async function(req,res){
+    return res.json({
+        "status": "ok",
+        "code": 202,
+        "message": "api found"
+    });
+});
 
 module.exports = app;
