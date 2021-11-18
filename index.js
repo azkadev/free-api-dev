acea0fc6c36949ec3cc881dae39aba3a5b6fc3a2
Azkadev <azkadev@pop-os.localdomain>
2021-11-18 08:37:45 +0700

    update

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+var route = require("./router/index");
+var port = process.env.PORT || 8080 || 3000 || 4040;
+
+app.set("json spaces", 2);
+app.use(express.json());
+app.use(express.static(__dirname + "/public"));
+app.use("/api", route.api);
+
+app.use("/", async function (req, res) {
+    var json = {
+        "status": "ok",
+        "code": 202,
+        "message": "server run normal"
+    };
+    return res.json(json);
+});
+
+app.listen(port, async function () {
+    console.log("server start on port " + port);
+});
